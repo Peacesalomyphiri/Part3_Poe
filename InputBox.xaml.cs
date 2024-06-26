99bf8685d5bec0ffef4c680f2f01edc5a4485754
Peace Salomy Phiri <st10163788@imconnect.edu.za>
2024-06-26 19:56:19 +0200

    Add project files.

---
+++ InputBox.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+
+namespace WpfRecipeApp
+{
+    public partial class InputBox : Window
+    {
+        public string InputText { get; private set; }
+
+        public InputBox(string prompt, string title = "Input")
+        {
+            InitializeComponent();
+            this.Title = title;
+            PromptTextBlock.Text = prompt;
+        }
+
+        private void OK_Click(object sender, RoutedEventArgs e)
+        {
+            this.InputText = InputTextBox.Text;
+            this.DialogResult = true;
+        }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = false;
+        }
+    }
+}
